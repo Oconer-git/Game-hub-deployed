4e11217aab8ea6953362319c2e9d5f4f2791fd13
Donell Oconer <donellpie@gmail.com>
2024-11-14 09:43:09 +0800

    game trailer gta

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,15 +1,14 @@
 import { useParams } from "react-router-dom"
 import useGame from "../hooks/useGame";
-import { Spinner, Heading, SimpleGrid, Text} from "@chakra-ui/react";
+import { Spinner, Heading} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {  
   const {slug} = useParams();
   const {data:game, isLoading, error} = useGame(slug!);
-  
+
   if(isLoading) return <Spinner/>
   if(error || !game) throw error;
 
@@ -18,6 +17,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game}/>
+      <GameTrailer gameId={game.id}/>
     </>
   )
 }
